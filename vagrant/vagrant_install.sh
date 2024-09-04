a8020d9cc1b6c8a874901f9a822c4e524f7ed86f
francisco-montilla <francisco-montilla@outlook.es>
2024-09-04 07:14:39 +0000

    Update sources on vagrant_install.sh

--- vagrant/vagrant_install.sh
+++ vagrant/vagrant_install.sh
@@ -1,12 +1,3 @@
 sudo apt-get install -y git
-
-mkdir /usr/share/BlueToolkit
-sudo chown $USER:$USER /usr/share/BlueToolkit
-
-git clone  https://github.com/sgxgsx/BlueToolkit /usr/share/BlueToolkit --recurse-submodules
-chmod +x /usr/share/BlueToolkit/install.sh
-/usr/share/BlueToolkit/install.sh
-
-
-
-
+git clone  https://github.com/xPecoide/BlueToolkit /usr/share/BlueToolkit --recurse-submodules
+chmod +x /usr/share/BlueToolkit/install.sh && /usr/share/BlueToolkit/install.sh
